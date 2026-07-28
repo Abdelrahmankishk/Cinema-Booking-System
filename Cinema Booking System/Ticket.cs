29f84e3292c0452974bac2a296a0e55c86710bfe
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-07-28 17:47:02 +0300

    add Ticket class , its properties and constructor casses uing contructor chaining

---
+++ Cinema Booking System/Ticket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Booking_System
+{
+    public enum TicketType
+    {
+        Standard,
+        VIP,
+        IMAX
+    }
+    public struct SeatLocation {
+        public char Row;
+        public int Column;
+    }
+    public class Ticket
+    {
+        public Ticket(string movieName, TicketType type, SeatLocation seat, double price)
+        {
+            MovieName = movieName;
+            Type = type;
+            Seat = seat;
+            Price = price;
+        }
+        public Ticket(string MovieName) :this(MovieName, TicketType.Standard, new SeatLocation { Row = 'A', Column = 1 }, 50)
+        {
+        }
+
+public string MovieName { get; set; }
+        public TicketType Type { get; set; }
+        public SeatLocation Seat { get; set; }
+        double Price { get; set; }
+
+        
+    }
+}
