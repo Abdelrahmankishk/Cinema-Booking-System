fedbb5fed7db73b58bbbd0b0feb9728996b848f1
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-07-28 22:00:26 +0300

    add `BookingHelper` a static utility class with some helping methods

---
+++ Cinema Booking System/BookingHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Booking_System
+{
+    public static class BookingHelper
+    {
+        static int Counter = 0;
+        public static double CalcGroupDiscount(int numberOfTickets, double pricePerTicket)
+        {
+            if(numberOfTickets >= 5)
+            {
+                return (numberOfTickets * pricePerTicket) * 0.9;
+            }
+            else
+            {
+                return numberOfTickets * pricePerTicket;
+            }
+        }
+
+        public static string GenerateBookingReference()
+        {
+            Counter++;
+            return $"BK-{Counter}";
+        }
+    }
+}
