e71090b89bd9401e602fcbaffb79be2d19936c81
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-07-28 21:24:27 +0300

    add a Cinema Class that holds multiple Tickets and added some features: - Allow User To get and set tickets by index - Allow User To Search Movie By movieName - a AddTicket method

---
+++ Cinema Booking System/Cinema.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Booking_System
+{
+    public class Cinema
+    {
+        Ticket[] _Tickets = new Ticket[20];
+
+        public Ticket this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _Tickets.Length)
+                {
+                    return null!;
+                }
+                return _Tickets[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _Tickets.Length)
+                {
+                    throw new IndexOutOfRangeException("Index is out of range.");
+                }
+                _Tickets[index] = value;
+            }
+        }
+        public Ticket this[string movieName]
+        {
+            get {
+                foreach (var ticket in _Tickets) { 
+                 if (ticket != null && ticket.MovieName == movieName)
+                    {
+                        return ticket;
+                    }
+                 else
+                    {
+                        return null!;
+                    }
+                }
+                return null!;
+            }
+        }
+
+        public bool AddTicket(Ticket t)
+        {
+            for (int  i = 0;  i < _Tickets.Length;  i++)
+            {
+                if( _Tickets[i] == null)
+                {
+                    _Tickets[i] = t;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
