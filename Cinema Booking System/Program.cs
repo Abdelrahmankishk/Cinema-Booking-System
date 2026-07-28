dffca2d2782c17d63fb4854c480735095b6bacd5
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-07-28 17:37:50 +0300

    Add project files.

---
+++ Cinema Booking System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema_Booking_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cinema Booking System");
+        }
+    }
+}
