17e1151fac62489b46c9b2119dc40cb6bab59538
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-08-02 11:57:30 +0300

    add a IMAXTicket class as a new Ticket option

---
+++ Cinema Booking System/IMAXTicket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Booking_System
+{
+    public class IMAXTicket : Ticket
+    {
+        bool Is3D;
+        public IMAXTicket(string movieName, double price, bool Is3D) : base(movieName, TicketType.IMAX, price)
+        {
+            if (Is3D) { 
+                price += 30;
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | IMAX 3D: {(Is3D ? "Yes" : "No")}";
+        }
+    }
+}
