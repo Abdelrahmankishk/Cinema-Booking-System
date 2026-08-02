ffb91a2ed6ea0582e2668c9afc55ff13f2ae0401
Abdelrahmankishk <abdelrahmankishk@gmail.com>
2026-08-02 11:54:04 +0300

    add VIPTicket class as a new type of ticket

---
+++ Cinema Booking System/VIPTicket.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema_Booking_System
+{
+    public class VIPTicket : Ticket
+    {
+        bool hasLoungeAccess;
+        decimal ServiceFee = 50.00m;
+        public VIPTicket(string movieName, double price, bool hasLoungeAccess, decimal ServiceFee) : base(movieName, TicketType.VIP, price)
+        {
+            this.hasLoungeAccess = hasLoungeAccess;
+            this.ServiceFee = ServiceFee;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $" | Lounge Access: {hasLoungeAccess} | Service Fee: {ServiceFee} EGP";
+        }
+    }
+}
